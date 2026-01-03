c83004f4a6fcb9397abd598ecb64e8cf94c0b29e
Ítalo Cosendey <italo_csv@hotmail.com>
2026-01-03 08:40:02 -0300

    Add database configuration for HostGator

---
+++ config/db_mysql_hostgator.php
@@ -0,0 +1,9 @@
+<?php
+// /home4/lojaur05/config/db_mysql_hostgator.php
+
+return [
+    'host' => 'br952.hostgator.com.br',
+    'db'   => 'lojaur05_tagplus',
+    'user' => 'lojaur05_admin',
+    'pass' => 'M2emsvjmt*20'
+];
