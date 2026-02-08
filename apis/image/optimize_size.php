f597b83414f7f54fcf17b9ce122368c6b04ba7a6
Ítalo Cosendey <italo_csv@hotmail.com>
2026-02-08 00:29:02 -0300

    melhoria na otimização de imagem

--- apis/image/optimize_size.php
+++ apis/image/optimize_size.php
@@ -8,7 +8,7 @@ if (!isset($_FILES['image'])) {
 $tmpIn  = $_FILES['image']['tmp_name'];
 $tmpOut = tempnam(sys_get_temp_dir(), 'img_') . '.jpg';
 
-$cmd = "ffmpeg -y -i {$tmpIn} -vf \"scale='min(2048,iw)':-2\" -q:v 3 -map_metadata -1 {$tmpOut}";
+$cmd = "ffmpeg -y -i {$tmpIn} -vf \"scale='min(2048,iw)':-2:flags=lanczos\" -q:v 2 -map_metadata -1 {$tmpOut}";
 exec($cmd);
 
 header('Content-Type: image/jpeg');
