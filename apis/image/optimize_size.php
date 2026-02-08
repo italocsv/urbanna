bc06e2a110a809f0e54cfe679facb369f5169334
Ítalo Cosendey <italo_csv@hotmail.com>
2026-02-08 09:16:38 -0300

    v2 imagem

--- apis/image/optimize_size.php
+++ apis/image/optimize_size.php
@@ -5,11 +5,26 @@ if (!isset($_FILES['image'])) {
   exit;
 }
 
+$limite = 160000; // 150 KB
 $tmpIn  = $_FILES['image']['tmp_name'];
 $tmpOut = tempnam(sys_get_temp_dir(), 'img_') . '.jpg';
 
+// 1️⃣ Se já é leve o suficiente, só retorna
+if (filesize($tmpIn) <= $limite) {
+    header('Content-Type: image/jpeg');
+    readfile($tmpIn);
+    exit;
+}
+
+// 2️⃣ Se já é leve o suficiente, só retorna
 $cmd = "ffmpeg -y -i {$tmpIn} -vf \"scale='min(2048,iw)':-2:flags=lanczos\" -q:v 2 -map_metadata -1 {$tmpOut}";
 exec($cmd);
+
+// 3️⃣ Se ainda passou do limite, cai a qualidade
+if (filesize($tmpOut) > $limite) {
+    $cmd = "ffmpeg -y -i {$tmpIn} -vf \"scale='min(2048,iw)':-2:flags=lanczos\" -q:v 3 -map_metadata -1 {$tmpOut}";
+    exec($cmd);
+}
 
 header('Content-Type: image/jpeg');
 readfile($tmpOut);
