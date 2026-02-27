fb983cfd75216d7f97cfdd5262235803cc3b6e87
Ítalo Cosendey <italo_csv@hotmail.com>
2026-02-26 22:00:57 -0300

    fix: update public base URL for video services

--- apis/services/videos/video-to-mp4.php
+++ apis/services/videos/video-to-mp4.php
@@ -8,7 +8,7 @@ $ffprobeBin = 'ffprobe';
 $baseDir = __DIR__;
 $tempDir = $baseDir . '/temp/';
 $outDir = $baseDir . '/runtime/videos/';
-$publicBaseUrl = 'https://services.urbanna.com.br/apps/videos/runtime/videos/';
+$publicBaseUrl = 'https://services.urbanna.com.br/apps/runtime/videos/';
 
 $maxDownloadBytes = 500 * 1024 * 1024; // 500MB
 
